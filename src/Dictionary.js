243d2d6e1864aaad41b2ef407c2cefca81784df4
Sarah Zahl <szahl@hotmail.co.uk>
2022-07-13 21:26:22 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Dictionary.css"
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState(null);
+    let [keyword, setKeyword] = useState("");
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
